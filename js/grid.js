660c32d9eaab59769c188b0cb60fc1cd5cb7c49c
bduimstra <me@brianduimstra.com>
2014-03-11 01:35:20 -0700

    Adding content styles and js redraw for vh and vw support

--- js/grid.js
+++ js/grid.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$('.js-nav-btn').on('click', function() {
 		$('.js-nav-btn, nav').toggleClass('visible');
 	});
+
+	// force redraw for vh and vw units on resize
+	$(window).on('resize', function() {
+		$('h1, h2, h3, p').css('z-index', '1');
+	});
 });
