15ec755f0b4760fb31567f0adc4c803462b27820
bduimstra <me@brianduimstra.com>
2014-03-13 22:10:10 -0700

    Styling updates

--- js/grid.js
+++ js/grid.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-	$('.js-nav-btn').on('click', function() {
-		$('.js-nav-btn, nav').toggleClass('visible');
-	});
+	function _init() {
+		$('.js-nav-btn').on('click', function() {
+			$('.js-nav-btn, nav').toggleClass('visible');
+		});
 
-	// force redraw for vh and vw units on resize
-	$(window).on('resize', function() {
-		$('h1, h2, h3, p').css('z-index', '1');
-	});
+		// force redraw for vh and vw units on resize
+		$(window).on('resize', function() {
+			$('h1, h2, h3, p').css('z-index', '1');
+		});
+
+		// resize the splash screen
+		$('main').css('height', $(window).height() - 60);
+	}
+
+	_init();
 });
